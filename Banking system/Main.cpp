a7deec63e4dc6c83a54d9ac42b6299845c049d81
MorawskiR <r.morawski92@gmail.com>
2020-08-27 00:05:54 +0200

    Update Main.cpp

--- Banking system/Main.cpp
+++ Banking system/Main.cpp
@@ -1,22 +1,21 @@
-#include <stdlib.h>
+#include <stdio.h> 
 #include <vector>
 #include <iostream>
+#include <stdlib.h>
 #include "Nagłówek.h"
 using namespace std;
 #include "windows.h" 
 int main()
 {
-	int a;
+
 	
 	do
 	{
 		MainMenu();
-		cout << "\nChoose your Option : "; cin >> wybor;
+		cout << "\nChoose your Option : ";
+		cin >> wybor;
 		Sleep(1000);
-		
 		system("cls");
 		menuwybor(wybor);
-		MainMenu();
-		cin >> a;
-	}	while (a !=0);
+	}	while (wybor);
 }
