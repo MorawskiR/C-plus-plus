ade7ca907857241ae7c2bb149cd69d64cf5773d9
MorawskiR <r.morawski92@gmail.com>
2020-09-02 01:07:37 +0200

    Create header.h

---
+++ casino/header.h
@@ -0,0 +1,19 @@
+#pragma once
+#include <iostream>
+#include <string>
+#include <iostream>
+using namespace std;
+
+extern string name;
+extern double deposit;
+extern int wybor;
+extern int Usernumber;
+extern double newDeposit;
+extern int random;
+void intro();
+void pobierzWartosci();
+void menu();
+void dice();
+void payment();
+void trafienie();
+void nietrafienie();
